60486b8ec9931ce11f43231972bffd4015b82cb8
Saksham <sakshamkushwaha12776@gmail.com>
2026-07-12 11:48:24 +0530

    feat: added theme

---
+++ frontend/src/components/mode-toggle.jsx
@@ -0,0 +1,40 @@
+import { Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/components/theme-provider";
+
+export function ModeToggle({ className }) {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    switch (theme) {
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("light");
+        break;
+      default:
+        // If theme is "system", switch based on the user's system preference
+        const prefersDark = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        ).matches;
+
+        setTheme(prefersDark ? "light" : "dark");
+    }
+  };
+
+  return (
+    <Button
+      aria-label="Toggle theme"
+      variant="outline"
+      size="icon"
+      className={className}
+      onClick={toggleTheme}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeProvider } from "@/components/theme-provider"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
+    </ThemeProvider>
   </StrictMode>
 );
