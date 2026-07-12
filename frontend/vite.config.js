042192e05b72954ce47773f8b772b7d08d39eb61
Saksham Kushwaha <92094239+lirena00@users.noreply.github.com>
2026-07-12 10:08:14 +0530

    Revert "Feat/shadcn"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+  plugins: [react()],
 });
